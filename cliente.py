95fe603a07293d541753e51fd9af34844bc2a33a
Natanael da Mota Figueira <54650675+natanaelmota1@users.noreply.github.com>
2023-01-26 13:53:40 -0400

    testando criptografia

---
+++ cliente.py
@@ -0,0 +1,142 @@
+import socket
+import random
+from threading import Thread
+from datetime import datetime
+from colorama import Fore, init, Back
+
+import random
+from math import pow
+
+a = random.randint(2, 10)
+
+def gcd(a, b):
+	if a < b:
+		return gcd(b, a)
+	elif a % b == 0:
+		return b
+	else:
+		return gcd(b, a % b)
+
+# Generating large random numbers
+def gen_key(q):
+
+	key = random.randint(pow(10, 20), q)
+	while gcd(q, key) != 1:
+		key = random.randint(pow(10, 20), q)
+
+	return key
+
+# Modular exponentiation
+def power(a, b, c):
+	x = 1
+	y = a
+
+	while b > 0:
+		if b % 2 != 0:
+			x = (x * y) % c
+		y = (y * y) % c
+		b = int(b / 2)
+
+	return x % c
+
+# Asymmetric encryption
+def encrypt(msg, q, h, g):
+
+	en_msg = []
+
+	k = gen_key(q)# Private key for sender
+	s = power(h, k, q)
+	p = power(g, k, q)
+	
+	for i in range(0, len(msg)):
+		en_msg.append(msg[i])
+
+	for i in range(0, len(en_msg)):
+		en_msg[i] = s * ord(en_msg[i])
+
+	return en_msg, p
+
+def decrypt(en_msg, p, key, q):
+
+	dr_msg = []
+	h = power(p, key, q)
+	for i in range(0, len(en_msg)):
+		dr_msg.append(chr(int(en_msg[i]/h)))
+		
+	return dr_msg
+
+q = random.randint(pow(10, 20), pow(10, 50))
+g = random.randint(2, q)
+key = gen_key(q)# Private key for receiver
+h = power(g, key, q)
+
+#######################################################################
+
+# init colors
+init()
+
+# set the available colors
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
+    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
+    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
+    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+]
+
+# choose a random color for the client
+client_color = random.choice(colors)
+
+# server's IP address
+# if the server is not on this machine, 
+# put the private (network) IP address (e.g 192.168.1.2)
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 5002 # server's port
+separator_token = "<SEP>" # we will use this to separate the client name & message
+
+# initialize TCP socket
+s = socket.socket()
+print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}...")
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+print("[+] Connected.")
+
+# prompt the client for a name
+name = input("Enter your name: ")
+
+def listen_for_messages():
+    while True:
+        message = s.recv(1024).decode()
+        msgEnc, keyP = message.split('-')
+        msgEnc = msgEnc.strip('[').strip(']').split(',')
+        for i in range(0, len(msgEnc)):
+            msgEnc[i] = int(msgEnc[i].strip(' '))
+
+        keyP = int(keyP)
+        dr_msg = decrypt(msgEnc, keyP, key, q)
+        dmsg = ''.join(dr_msg)
+        print("\n" + dmsg)
+        print(keyP)
+
+# make a thread that listens for messages to this client & print them
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
+
+while True:
+    # input message we want to send to the server
+    to_send =  input()
+    en_msg, p = encrypt(to_send, q, h, g)
+    msgEncrypt = str(en_msg) + "-" + str(p)
+
+    # a way to exit the program
+    if to_send.lower() == 'q':
+        break
+    # add the datetime, name & the color of the sender
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+    # finally, send the message
+    s.send(msgEncrypt.encode())
+
+# close the socket
+s.close()
